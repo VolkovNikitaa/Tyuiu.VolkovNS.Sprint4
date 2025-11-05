c6c411c3d887af23aba2f726c07d58ae529885ff
Nikita <Nikita@DESKTOP-BFU5003>
2025-11-05 19:32:46 +0300

    Task3

--- Tyuiu.VolkovNS.Sprint4.Task3.V20.Test/DataServiceTest.cs
+++ Tyuiu.VolkovNS.Sprint4.Task3.V20.Test/DataServiceTest.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.VolkovNS.Sprint4.Task3.V20.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-            int[] array = new int[] { 2, 4, 4, 7, 2, 5, 4, 3, 3, 4 };
+            int[,] array = new int[5, 5] { { 8, 7, 7, 8, 5 }, { 4, 3, 5, 3, 6 }, { 5, 3, 8, 6, 3 }, { 6, 3, 8, 5, 4 }, { 3, 6, 8, 3, 4 } };
             int sum = 1024;
             Assert.AreEqual(ds.Calculate(array), sum);
         }
