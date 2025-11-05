6452a3872b39f060bf1b97227d1f2f4fe6cb8264
Nikita <Nikita@DESKTOP-BFU5003>
2025-11-05 19:22:33 +0300

    Task2

---
+++ Tyuiu.VolkovNS.Sprint4.Task2.V22/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.VolkovNS.Sprint4.Task2.V22.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил Волков Н. С. | ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                    *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнил: Волков Никита Сергеевич | ПКТб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Подсчитать произведение нечётных элементов массива                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int[] array = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToInt32(rand.Next());
+        }
+        Console.WriteLine("Ответ: " + ds.Calculate(array));
+    }}
+}
